6059e8cb10c7b0961df5d5fddb3d9c61140f757d
Khoeru Roziqin <roziqinkhoeru8@gmail.com>
2024-05-24 14:11:17 +0700

    `chore` update types

---
+++ src/types/index.ts
@@ -0,0 +1,33 @@
+type DogImg = {
+  id: string;
+  width: number;
+  height: number;
+  url: string;
+};
+
+interface Dogs {
+  bred_for: string;
+  breed_group: string;
+  height: { imperial: string; metric: string };
+  id: number;
+  life_span: string;
+  name: string;
+  origin?: string;
+  reference_image_id: string;
+  temperament: string;
+  weight: { imperial: string; metric: string };
+}
+
+interface DogImages {
+  id: string;
+  url: string;
+  breeds: Dogs[];
+  width: string;
+  height: string;
+}
+
+interface DogBreeds extends Dogs {
+  image: DogImg;
+}
+
+export type { DogBreeds, DogImages, Dogs };
