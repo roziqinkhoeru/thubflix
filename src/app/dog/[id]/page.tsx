3e47e0170f07bb65ecb82d71b314cb2cc99f30dc
Khoeru Roziqin <roziqinkhoeru8@gmail.com>
2024-05-24 14:11:46 +0700

    `feat(dog)` update dogs view

--- src/app/dog/[id]/page.tsx
+++ src/app/dog/[id]/page.tsx
@@ -1,22 +1,23 @@
 import ButtonBack from "@/components/shared/ButtonBack";
+import { Badge } from "@/components/ui/badge";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { DogImages, Dogs } from "@/types";
+import { Metadata } from "next";
+import Image from "next/image";
 
-interface Dogs {
-  bred_for: string;
-  breed_group: string;
-  height: { imperial: string; metric: string };
-  id: number;
-  image: {
-    id: string;
-    width: number;
-    height: number;
-    url: string;
+export async function generateMetadata({
+  params,
+}: {
+  params: { id: string };
+}): Promise<Metadata> {
+  const id = params.id;
+
+  const dogs = await fetchDog(id);
+
+  return {
+    title: dogs.name,
   };
-  life_span: string;
-  name: string;
-  origin: string;
-  reference_image_id: string;
-  temperament: string;
-  weight: { imperial: string; metric: string };
 }
 
 const fetchDog = async (id: string) => {
@@ -28,11 +29,6 @@ const fetchDog = async (id: string) => {
 
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_DOG_API}/breeds/${id}`,
-      {
-        headers: {
-          "x-api-key": apiKey,
-        },
-      },
     );
 
     const data: Dogs = await response.json();
@@ -42,8 +38,34 @@ const fetchDog = async (id: string) => {
   }
 };
 
+const fetchDogImage = async (imageId: string) => {
+  try {
+    const apiKey = process.env.NEXT_PUBLIC_DOG_API_KEY;
+    if (!apiKey) {
+      throw new Error("API key is missing");
+    }
+
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_DOG_API}/images/${imageId}`,
+    );
+
+    const data: DogImages = await response.json();
+    return data;
+  } catch (error) {
+    throw new Error("Failed to fetch dog");
+  }
+};
+
 async function page({ params }: { params: { id: string } }) {
   const dogs = await fetchDog(params.id);
+  const dogImage = await fetchDogImage(dogs.reference_image_id);
+  const listOfBreedGroup =
+    dogs.breed_group !== "" ? dogs.breed_group?.split(",") : null;
+  const listOfBreedFor =
+    dogs.bred_for !== "" ? dogs.bred_for?.split(",") : null;
+  const listOfTemperament =
+    dogs.temperament !== "" ? dogs.temperament?.split(",") : null;
+  const listOfOrigin = dogs.origin !== "" ? dogs.origin?.split(",") : null;
 
   return (
     <main className="container mx-auto mt-10 max-w-7xl px-6">
@@ -51,10 +73,133 @@ async function page({ params }: { params: { id: string } }) {
         <div className="mb-6">
           <ButtonBack />
         </div>
-        <div>{dogs.name}</div>
-        <div>{dogs.breed_group}</div>
-        <div>{dogs.temperament}</div>
-        <div>{dogs.bred_for}</div>
+        <figure className="w-52">
+          <Image
+            src={dogImage.url}
+            alt={dogs.name}
+            width={200}
+            height={200}
+            className="mb-6 block h-auto w-full rounded-md"
+          />
+        </figure>
+        <div className="mb-4 grid w-full max-w-sm items-center gap-1.5">
+          <Label htmlFor="name">Name</Label>
+          <Input
+            type="text"
+            id="name"
+            placeholder="Name"
+            value={dogs.name}
+            readOnly
+          />
+        </div>
+        <div className="mb-4 grid w-full max-w-sm items-center gap-1.5">
+          <Label htmlFor="breedGroup">Breed Group</Label>
+          <div className="mt-1 flex flex-wrap items-center gap-1.5">
+            {listOfBreedGroup
+              ? listOfBreedGroup?.map((breedGroup, index) => (
+                  <Badge
+                    key={index}
+                    variant="outline"
+                    className="text-slate-700"
+                  >
+                    {breedGroup}
+                  </Badge>
+                ))
+              : "-"}
+          </div>
+        </div>
+        <div className="mb-4 grid w-full max-w-sm items-center gap-1.5">
+          <Label htmlFor="breedFor">Breed For</Label>
+          <div className="mt-1 flex flex-wrap items-center gap-1.5">
+            {listOfBreedFor
+              ? listOfBreedFor?.map((breedFor, index) => (
+                  <Badge
+                    key={index}
+                    variant="outline"
+                    className="text-slate-700"
+                  >
+                    {breedFor}
+                  </Badge>
+                ))
+              : "-"}
+          </div>
+        </div>
+        <div className="mb-4 grid w-full max-w-sm items-center gap-1.5">
+          <Label htmlFor="temper">Temperament</Label>
+          <div className="mt-1 flex flex-wrap items-center gap-1.5">
+            {listOfTemperament
+              ? listOfTemperament?.map((temper, index) => (
+                  <Badge
+                    key={index}
+                    variant="outline"
+                    className="text-slate-700"
+                  >
+                    {temper}
+                  </Badge>
+                ))
+              : "-"}
+          </div>
+        </div>
+        <div className="mb-4 grid w-full max-w-sm items-center gap-1.5">
+          <Label htmlFor="origin">Origin</Label>
+          <div className="mt-1 flex flex-wrap items-center gap-1.5">
+            {listOfOrigin
+              ? listOfOrigin?.map((original, index) => (
+                  <Badge
+                    key={index}
+                    variant="outline"
+                    className="text-slate-700"
+                  >
+                    {original}
+                  </Badge>
+                ))
+              : "-"}
+          </div>
+        </div>
+        <div className="mb-4 grid w-full max-w-sm items-center gap-1.5">
+          <Label htmlFor="lifeSpan">Life Span</Label>
+          <Input
+            type="text"
+            id="lifeSpan"
+            placeholder="Life Span"
+            value={dogs.life_span}
+            readOnly
+          />
+        </div>
+        <div className="mb-4 grid w-full max-w-sm items-center gap-1.5">
+          <Label htmlFor="weight">Weight</Label>
+          <Input
+            type="text"
+            id="weightImperials"
+            placeholder="Weight Imperials"
+            value={`${dogs.weight.imperial} (imperials)`}
+            readOnly
+          />
+          <Input
+            type="text"
+            id="weightMetric"
+            placeholder="Weight Metric"
+            value={`${dogs.weight.metric} (metric)`}
+            readOnly
+          />
+        </div>
+        <div className="mb-4 grid w-full max-w-sm items-center gap-1.5">
+          <Label htmlFor="height">Height</Label>
+          <Input
+            type="text"
+            id="heightImperial"
+            placeholder="Height Imperial"
+            value={`${dogs.height.imperial} (imperial)`}
+            readOnly
+          />
+          <Input
+            type="text"
+            id="heightMetric"
+            placeholder="Height Metric"
+            value={`${dogs.height.metric} (metric)`}
+            readOnly
+          />
+        </div>
       </div>
     </main>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,71 +1,22 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-
-import * as React from "react";
-
-
+import Link from "next/link";
 
 export default function Home() {
-
-
   return (
     <main className="container mx-auto mt-10 max-w-7xl px-6">
       <section className="mx-auto mb-12 w-full">
-        <Card className="w-[350px]">
-          <CardHeader>
-            <CardTitle>Create project</CardTitle>
-            <CardDescription>
-              Deploy your new project in one-click.
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            <form>
-              <div className="grid w-full items-center gap-4">
-                <div className="flex flex-col space-y-1.5">
-                  <Label htmlFor="name">Name</Label>
-                  <Input id="name" placeholder="Name of your project" />
-                </div>
-                <div className="flex flex-col space-y-1.5">
-                  <Label htmlFor="framework">Framework</Label>
-                  <Select>
-                    <SelectTrigger id="framework">
-                      <SelectValue placeholder="Select" />
-                    </SelectTrigger>
-                    <SelectContent position="popper">
-                      <SelectItem value="next">Next.js</SelectItem>
-                      <SelectItem value="sveltekit">SvelteKit</SelectItem>
-                      <SelectItem value="astro">Astro</SelectItem>
-                      <SelectItem value="nuxt">Nuxt.js</SelectItem>
-                    </SelectContent>
-                  </Select>
-                </div>
-              </div>
-            </form>
-          </CardContent>
-          <CardFooter className="flex justify-between">
-            <Button variant="outline">Cancel</Button>
-            <Button>Deploy</Button>
-          </CardFooter>
-        </Card>
+        <div className="flex h-[calc(100svh-10rem)] min-h-96 w-full items-center justify-center">
+          <div className="text-center">
+            <h1 className="text-2xl font-bold">
+              <span className="text-red-700">Dogland</span>: Discover Dog Breeds
+            </h1>
+            <Button asChild className="mt-6">
+              <Link href="/dog">Explore</Link>
+            </Button>
+          </div>
+        </div>
       </section>
     </main>
   );
